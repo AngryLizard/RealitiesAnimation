98b7a72a8ca00444fc2c30d6694c987be97ac6bf
Angry Lizard <gitlab@netliz.net>
2022-09-09 18:55:01 +0200

    initial commit

---
+++ Source/RealitiesAnimationEditor/Modifiers/TGOR_ModifierMirror.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+#include "CoreMinimal.h"
+
+class ISequencer;
+
+struct FTGOR_ModifierMirror
+{
+	TWeakPtr<ISequencer> GetSequencer() const;
+
+	void Mirror();
+	void Loop();
+	void TimeOffsetHalf();
+
+	void FlipLeftToRight(bool bWithOffet);
+	void FlipRightToLeft(bool bWithOffet);
+};
+
+
